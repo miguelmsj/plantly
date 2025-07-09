fe84e625041ae327b0d3fa7fcbdaf04fa2a6e23a
Miguel San Juan <miguelmsanjuan@gmail.com>
2025-07-09 18:09:00 -0300

    Refactor navigation structure and add onboarding flow
    - Replaced Tabs with Stack navigation in _layout.tsx for improved routing.
    - Introduced OnboardingScreen component for user onboarding.
    - Created new layout for tabs with conditional redirect based on onboarding status.
    - Added Profile and index screens under the new tabs structure.

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,37 @@
+import { Redirect, Tabs } from "expo-router";
+import Entypo from "@expo/vector-icons/Entypo";
+import Feather from "@expo/vector-icons/Feather";
+import { theme } from "@/theme";
+
+const hasFinishedOnboarding = false;
+
+export default function Layout() {
+  if (!hasFinishedOnboarding) {
+    return <Redirect href="/onboarding" />;
+  }
+
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colors.green }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="leaf" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="user" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
